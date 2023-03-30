4d83e814753984250e3fb42ea0069dd0d83aecde
RagnarSmari <raggism98@gmail.com>
2023-03-30 10:47:41 +0000

    Added to level, Changed main camera

---
+++ Assets/Scripts/LevelManager1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the Player goes below -500 on the y-axis reload the scene
+        if (Player.position.y <= -60)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
